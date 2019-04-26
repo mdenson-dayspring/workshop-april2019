5619be65763dc3464ed3e11c3b060c73bfd70d2a
Matthew Denson <mdenson@dayspring-tech.com>
2019-04-26 16:40:26 -0700

    error handling in Observables
    Worked out the error handling in the Observable case.

--- src/observables/index.ts
+++ src/observables/index.ts
@@ -1,5 +1,5 @@
-import { timer, Observable } from 'rxjs';
-import { map, switchMap, tap, filter } from 'rxjs/operators';
+import { timer, Observable, throwError, of } from 'rxjs';
+import { switchMap, catchError, tap, filter } from 'rxjs/operators';
 
 interface User {
   name: string;
@@ -9,21 +9,44 @@ class Observables {
   checkAuth() {
     console.log("check auth...");
     return timer(2000).pipe(
-      map(() => true)
+      // switchMap(() => throwError('Bad Password.'))
+      switchMap(() => of(true))
     );
   }
 
   getUser(): Observable<User> {
     console.log("get user...");
     return timer(2000).pipe(
-      map(() => { return { name: "Max" }; })
+      // switchMap(() => throwError('Bad User ID.'))
+      switchMap(() => of({name: 'Max'}))
     );
   }
 }
 
+const filterOnError = (callback) => source => {
+  return source.pipe(
+      catchError(err => {
+        callback(err);
+        return of(undefined);
+      }),
+      filter(val => val!==undefined)
+    );
+}
+
 const srv = new Observables();
 srv.checkAuth().pipe(
-  filter(auth => auth),
-  switchMap(() => srv.getUser()),
-  tap(user => console.log(user.name))
-).subscribe();
+  filterOnError(err => console.log('Authentication Error: ' + err) ),
+  switchMap(auth => {
+    if (auth) {
+      return srv.getUser();
+    } else {
+      return throwError('Unexpected error.');
+    }
+  }),
+  tap((val: User) => console.log(val.name)),
+  filterOnError(err => console.log('Data Access Error: ' + err) )
+).subscribe(
+  next=> console.log('next', next),
+  err=> console.log('error', err),
+  ()=>console.log('Completed.')
+);
